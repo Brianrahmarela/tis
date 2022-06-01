8f84841254f95445cba8561e23400473afd76702
Brianrahmarela <brian.rahmarela@gmail.com>
2022-06-01 19:06:37 +0700

    add cart oval hat item

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -2,129 +2,135 @@ import React, { useState } from "react";
 import { Col, Card, Container } from "react-bootstrap";
 import { numberWithCommas } from "../utils/utils";
 const Items = ({ item, addCart }) => {
-  // console.log("item", item);
-  // console.log("addCart", addCart);
-  const dataLocalStorage = localStorage.getItem("dataUser");
-  const data = JSON.parse(dataLocalStorage);
-  // console.log('data local', data)
+	// console.log("item", item);
+	// console.log("addCart", addCart);
+	const dataLocalStorage = localStorage.getItem("dataUser");
+	const data = JSON.parse(dataLocalStorage);
+	// console.log('data local', data)
 
-  let [qty, setQty] = useState(0);
-  console.log("STATE qty", qty);
+	let [qty, setQty] = useState(0);
+	// console.log("STATE qty", qty);
 
-  let filteredVIP = item.prices.filter(
-    (item, index) => item.priceFor !== "wholesale"
-  );
-  // console.log("filteredVIP", filteredVIP);
+	let filteredVIP = item.prices.filter(
+		(item, index) => item.priceFor !== "wholesale"
+	);
+	// console.log("filteredVIP", filteredVIP);
 
-  let filteredWhole = item.prices.filter(
-    (item, index) => item.priceFor !== "VIP"
-  );
-  // console.log("filteredWhole", filteredWhole);
+	let filteredWhole = item.prices.filter(
+		(item, index) => item.priceFor !== "VIP"
+	);
+	// console.log("filteredWhole", filteredWhole);
 
-  let filteredRegular = item.prices.filter(
-    (item, index) => item.priceFor === "regular"
-  );
-  // console.log("filteredWhole", filteredWhole);
+	let filteredRegular = item.prices.filter(
+		(item, index) => item.priceFor === "regular"
+	);
+	// console.log("filteredWhole", filteredWhole);
 
-  let handeQtyPlus = () => {
-    setQty((qty += 1));
-  };
-  let handeQtyMin = () => {
-    if (qty > 0) {
-      setQty((qty -= 1));
-    }
-  };
+	let handeQtyPlus = () => {
+		setQty((qty += 1));
+		addCart(
+			item,
+			data.type === "VIP"
+				? filteredVIP
+				: data.type === "wholesale"
+				? filteredWhole
+				: filteredRegular,
+			qty,
+			setQty
+		);
+	};
+	let handeQtyMin = () => {
+		if (qty > 0) {
+			setQty((qty -= 1));
+			addCart(
+				item,
+				data.type === "VIP"
+					? filteredVIP
+					: data.type === "wholesale"
+					? filteredWhole
+					: filteredRegular,
+				qty,
+				setQty
+			);
+		}
+	};
 
-  return (
-    <Col>
-      <Card
-        className="shadow mb-4"
-        onClick={() =>
-          addCart(
-            item,
-            data.type === "VIP"
-              ? filteredVIP
-              : data.type === "wholesale"
-              ? filteredWhole
-              : filteredRegular,
-            qty,
-            setQty
-          )
-        }
-      >
-        <Card.Img variant="top" src={item.image} />
-        <Card.Body>
-          <Card.Title>
-            <strong>{item.name}</strong>
-          </Card.Title>
-          <Card.Text>{item.description}</Card.Text>
-          {data.type === "VIP"
-            ? filteredVIP.map((prices, index) => (
-                <Card.Text key={index}>
-                  {prices.priceFor === "VIP" ? (
-                    <strong>Rp. {numberWithCommas(prices.price)}</strong>
-                  ) : (
-                    <>Rp. {numberWithCommas(prices.price)}</>
-                  )}{" "}
-                  {prices.priceFor === "VIP" ? (
-                    <strong>({prices.priceFor})</strong>
-                  ) : (
-                    prices.priceFor
-                  )}
-                </Card.Text>
-              ))
-            : data.type === "wholesale"
-            ? filteredWhole.map((prices, index) => (
-                <Card.Text key={index}>
-                  {prices.priceFor === "wholesale" ? (
-                    <strong>Rp. {numberWithCommas(prices.price)}</strong>
-                  ) : (
-                    <>Rp. {numberWithCommas(prices.price)}</>
-                  )}{" "}
-                  {prices.priceFor === "wholesale" ? (
-                    <strong>({prices.priceFor})</strong>
-                  ) : (
-                    prices.priceFor
-                  )}
-                </Card.Text>
-              ))
-            : filteredRegular.map((prices, index) => (
-                <>
-                  <Card.Text key={index}>
-                    {prices.priceFor === "regular" ? (
-                      <strong>Rp. {numberWithCommas(prices.price)}</strong>
-                    ) : (
-                      <>Rp. {numberWithCommas(prices.price)}</>
-                    )}{" "}
-                    {prices.priceFor === "regular" ? (
-                      <strong>({prices.priceFor})</strong>
-                    ) : (
-                      prices.priceFor
-                    )}
-                  </Card.Text>
-                </>
-              ))}
-          <div className="d-flex flex-row align-items-center pr-2">
-            <div style={{ paddingRight: 10 }}>
-              {" "}
-              <button onClick={handeQtyPlus}>
-                <strong>+</strong>
-              </button>
-            </div>
-            <div style={{ paddingRight: 10 }}>
-              <h5 style={{ margin: 0, padding: 0 }}>{qty}</h5>
-            </div>
-            <div>
-              {" "}
-              <button onClick={handeQtyMin}>
-                <strong>-</strong>
-              </button>
-            </div>
-          </div>
-        </Card.Body>
-      </Card>
-    </Col>
-  );
+	return (
+		<Col>
+			<Card className="shadow mb-4">
+				<Card.Img variant="top" src={item.image} />
+				<Card.Body>
+					<Card.Title>
+						<strong>{item.name}</strong>
+					</Card.Title>
+					<Card.Text>{item.description}</Card.Text>
+					{data.type === "VIP"
+						? filteredVIP.map((prices, index) => (
+								<Card.Text key={index}>
+									{prices.priceFor === "VIP" ? (
+										<strong>Rp. {numberWithCommas(prices.price)}</strong>
+									) : (
+										<>Rp. {numberWithCommas(prices.price)}</>
+									)}{" "}
+									{prices.priceFor === "VIP" ? (
+										<strong>({prices.priceFor})</strong>
+									) : (
+										prices.priceFor
+									)}
+								</Card.Text>
+						  ))
+						: data.type === "wholesale"
+						? filteredWhole.map((prices, index) => (
+								<Card.Text key={index}>
+									{prices.priceFor === "wholesale" ? (
+										<strong>Rp. {numberWithCommas(prices.price)}</strong>
+									) : (
+										<>Rp. {numberWithCommas(prices.price)}</>
+									)}{" "}
+									{prices.priceFor === "wholesale" ? (
+										<strong>({prices.priceFor})</strong>
+									) : (
+										prices.priceFor
+									)}
+								</Card.Text>
+						  ))
+						: filteredRegular.map((prices, index) => (
+								<>
+									<Card.Text key={index}>
+										{prices.priceFor === "regular" ? (
+											<strong>Rp. {numberWithCommas(prices.price)}</strong>
+										) : (
+											<>Rp. {numberWithCommas(prices.price)}</>
+										)}{" "}
+										{prices.priceFor === "regular" ? (
+											<strong>({prices.priceFor})</strong>
+										) : (
+											prices.priceFor
+										)}
+									</Card.Text>
+								</>
+						  ))}
+					<div className="d-flex flex-row align-items-center pr-2">
+						<div style={{ paddingRight: 10 }}>
+							{" "}
+							<button onClick={handeQtyPlus}>
+								<strong>+</strong>
+							</button>
+						</div>
+						<div style={{ paddingRight: 10 }}>
+							<h5 style={{ margin: 0, padding: 0 }}>{qty}</h5>
+						</div>
+						<div>
+							{" "}
+							<button onClick={handeQtyMin}>
+								<strong>-</strong>
+							</button>
+						</div>
+					</div>
+				</Card.Body>
+			</Card>
+		</Col>
+	);
 };
 
 export default Items;
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -6,122 +6,222 @@ import axios from "axios";
 import swal from "sweetalert";
 
 export default class Dashboard extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      items: [],
-      isLoading: true,
-      cart: [],
-      isLogin: false,
-    };
-  }
+		this.state = {
+			items: [],
+			isLoading: true,
+			cart: [],
+			isLogin: false,
+			cekTrans: []
+		};
+	}
 
-  componentDidMount() {
-    axios
-      .get(`${API_URL}/Items`)
-      .then((res) => {
-        const items = res.data;
-        this.setState({ items: items, isLoading: false });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
+	componentDidMount() {
+		axios
+			.get(`${API_URL}/Items`)
+			.then((res) => {
+				// console.log("res Items", res);
 
-  addCart = (value, price, qty, setQty) => {
-    const dataLocalStorage = localStorage.getItem("dataUser");
-    const data = JSON.parse(dataLocalStorage);
-    // console.log("data user addCart", data);
-    // console.log("value", value);
-    console.log("price msk addCart", price);
-    console.log("price msk qty", qty);
-    console.log("price msk setQty", setQty);
+				const items = res.data;
+				this.setState({ items: items, isLoading: false });
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}
 
-    let filteredPriceType = price.filter(
-      (item, index) => item.priceFor === data.type
-    );
-    // console.log("filteredPriceType", filteredPriceType);
+	addCart = (value, price, qty, setQty) => {
+		const dataLocalStorage = localStorage.getItem("dataUser");
+		const data = JSON.parse(dataLocalStorage);
+		// console.log("data user addCart", data);
+		console.log("value", value);
+		console.log("price msk addCart", price);
+		// console.log("price msk qty", qty);
+		// console.log("price msk setQty", setQty);
 
-    let filterPrice = 0;
-    filteredPriceType.forEach((element) => {
-      return (filterPrice = element.price);
-    });
-    // console.log("filterPrice", filterPrice);
+		let filteredPriceType = price.filter(
+			(item, index) => item.priceFor === data.type
+		);
+		// console.log("filteredPriceType", filteredPriceType);
 
-    let filteredPriceDefault = price.filter(
-      (item, index) => item.priceFor !== data.type
-    );
-    // console.log("filteredPriceDefault", filteredPriceDefault);
-    
-    let filterDefaultPrice = 0;
-    filteredPriceDefault.forEach((element) => {
-      return (filterDefaultPrice = element.price);
-    });
-    // console.log("filterDefaultPrice", filterDefaultPrice);
-    
-    
+		let filterPrice = 0;
+		filteredPriceType.forEach((element) => {
+			return (filterPrice = element.price);
+		});
+		// console.log("filterPrice", filterPrice);
 
-    let cart = {
-      qty: qty,
-      price_account: filterPrice ? filterPrice : filterDefaultPrice,
-    }
+		let filteredPriceDefault = price.filter(
+			(item, index) => item.priceFor !== data.type
+		);
+		// console.log("filteredPriceDefault", filteredPriceDefault);
 
-    cart = {
-      id: value.id,
-      item: value,
-      qty: qty,
-      buyer: data.username,
-      price_account: filterPrice ? filterPrice : filterDefaultPrice,
-      totalPrice: cart.price_account * cart.qty
-    }
-    console.log('cart', cart)
-    axios
-    .post(`${API_URL}/Transaction`, cart)
-    .then((res) => {
-      if(res.data.length === 0){
+		let filterDefaultPrice = 0;
+		filteredPriceDefault.forEach((element) => {
+			return (filterDefaultPrice = element.price);
+		});
+		// console.log("filterDefaultPrice", filterDefaultPrice);
 
-        swal({
-          title: "Sukses Masuk Keranjang!",
-          text: `Produk ${cart.item.name} sukses Masuk Keranjang`,
-          icon: "success",
-          button: false,
-        });
-      }
+		let cart = {
+			qty: qty,
+			price_account: filterPrice ? filterPrice : filterDefaultPrice
+		};
 
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-  };
+		cart = {
+			id: value.id,
+			item: value,
+			qty: qty,
+			buyer: data.username,
+			price_account: filterPrice ? filterPrice : filterDefaultPrice,
+			totalPrice: cart.price_account * cart.qty
+		};
+		console.log("cart", cart);
+		console.log("this.cekTrans", this.state.cekTrans);
+		console.log("cart qtyy", cart.qty);
 
-  render() {
-    const { items, isLoading } = this.state;
-    // console.log(this.state);
-    return (
-      <div className="mt-3">
-        <Container fluid>
-          <Row>
-            <ListCategories />
-            <Col>
-              <h4>
-                <strong>Daftar Produk</strong>
-              </h4>
-              <hr />
-              <Row>
-                {items && isLoading === false ? (
-                  items.map((item, index) => (
-                    <Items key={item.name} item={item} addCart={this.addCart} />
-                  ))
-                ) : (
-                  <Spinner animation="border" />
-                )}
-              </Row>
-            </Col>
-            <Result />
-          </Row>
-        </Container>
-      </div>
-    );
-  }
+		if (cart.qty === 1 && this.state.cekTrans.length === 0) {
+			// console.log("qty <= 1");
+			console.log("mskk qtyy", qty);
+			// console.log(
+			// 	cart.qty === 1 && this.state.cekTrans === this.state.cekTrans
+			// );
+			console.log(cart.qty === 1 && this.state.cekTrans.length === 0);
+			console.log("this.cekTrans", this.state.cekTrans);
+
+			axios
+				.post(`${API_URL}/Transaction`, cart)
+				.then((res) => {
+					console.log("res", res);
+					if (res.status === 201) {
+						swal({
+							title: "Sukses Masuk Keranjang!",
+							text: `Produk ${cart.item.name} sukses Masuk Keranjang`,
+							icon: "success",
+							button: false
+						});
+					}
+					const items = res.data;
+					this.setState((prevState) => ({
+						cekTrans: [...prevState.cekTrans, items],
+						isLoading: false
+					}));
+				})
+				.catch((error) => {
+					console.log(error);
+				});
+		} else if (cart.qty === 1 && this.state.cekTrans.length !== 0) {
+			console.log("MASUKKKK!!");
+			console.log(cart.qty === 1 && this.state.cekTrans.length !== 0);
+			axios
+				.put(`${API_URL}/Transaction/${cart.id}`, cart)
+
+				.then((res) => {
+					console.log("res + qty", res);
+					const items = res.data;
+					this.setState((prevState) => ({
+						cekTrans: [...prevState.cekTrans, items],
+						isLoading: false
+					}));
+				})
+				.catch((error) => {
+					console.log(error);
+				});
+			if (cart.qty === cart.qty + 1) {
+				console.log("plus 1!");
+			}
+		} else if (qty > 1) {
+			console.log("qty > 1");
+			console.log("qtyy", qty);
+
+			cart = {
+				id: value.id,
+				item: value,
+				qty: qty,
+				buyer: data.username,
+				price_account: filterPrice ? filterPrice : filterDefaultPrice,
+				totalPrice: cart.price_account * cart.qty
+			};
+			console.log("cart patch", cart);
+			axios
+				.put(`${API_URL}/Transaction/${cart.id}`, cart)
+
+				.then((res) => {
+					console.log("res + qty", res);
+					const items = res.data;
+					this.setState((prevState) => ({
+						cekTrans: [...prevState.cekTrans, items],
+						isLoading: false
+					}));
+				})
+				.catch((error) => {
+					console.log(error);
+				});
+		} else if (qty === 0) {
+			console.log("qty == 0");
+			console.log("qtyy", qty);
+			axios
+				.delete(`${API_URL}/Transaction/${cart.id}`)
+				.then((res) => {
+					console.log("res qty == 0", res);
+					// const items = res.data;
+					this.setState({ cekTrans: [], isLoading: false });
+				})
+				.catch((error) => {
+					console.log(error);
+				});
+		} else {
+			axios
+				.post(`${API_URL}/Transaction`, cart)
+				.then((res) => {
+					console.log("res", res);
+					if (res.status === 201) {
+						swal({
+							title: "Sukses Masuk Keranjang!",
+							text: `Produk ${cart.item.name} sukses Masuk Keranjang`,
+							icon: "success",
+							button: false
+						});
+					}
+					const items = res.data;
+					this.setState((prevState) => ({
+						cekTrans: [...prevState.cekTrans, items],
+						isLoading: false
+					}));
+				})
+				.catch((error) => {
+					console.log(error);
+				});
+		}
+	};
+
+	render() {
+		const { items, isLoading } = this.state;
+		console.log("state", this.state);
+		return (
+			<div className="mt-3">
+				<Container fluid>
+					<Row>
+						<ListCategories />
+						<Col>
+							<h4>
+								<strong>Daftar Produk</strong>
+							</h4>
+							<hr />
+							<Row>
+								{items && isLoading === false ? (
+									items.map((item, index) => (
+										<Items key={item.name} item={item} addCart={this.addCart} />
+									))
+								) : (
+									<Spinner animation="border" />
+								)}
+							</Row>
+						</Col>
+						<Result />
+					</Row>
+				</Container>
+			</div>
+		);
+	}
 }
